ab65499077c654833d7e14d00092e225fd8f7ceb
DanWeitsman <dfw5266@psu.edu>
2022-11-29 17:37:48 -0500

    cleaned up the test optimization script opt.py

--- opt.py
+++ opt.py
@@ -3,12 +3,61 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import resonator as res
 import os
-import re
 from scipy.signal import butter
 from scipy.optimize import differential_evolution,NonlinearConstraint
 import sys
-sys.path.insert(0, '/Users/danielweitsman/codes/github/DanWeitsman/pyPostAcs')
+from time import time
+sys.path.insert(0, os.path.join(os.path.dirname(os.getcwd()),'pyPostAcs'))
 import pyPostAcsFun as fun
+
+#%%
+
+def init_res(f,a_n,L_n,a_c,L_c):
+    '''
+    This function creates and returns a resonator object with its complex impedance evaluated
+    '''
+    res_temp = res.resonator(a_n = a_n,L_n =L_n,a_c = a_c, L_c = L_c)
+    res_temp.set_Z(f,model = 'k',rad = False,interior = False,loss = True,table = False)
+    return res_temp
+
+def smeared_Z(f,res_params):
+    '''
+    This function computes the smeared impedance and absorption for a sample that consists of multiple resonator cavities that have different geometries. 
+    
+    Parameters:
+    f: frequency array [Hz]
+    res_params: a nested array whose elements corresponding to each resonator in the sample. Each of the nested arrays must be of size (5,) and specifies
+    [[# of this resonator in the sample, a_n,L_n,a_c,L_c]]
+
+    Return:
+    Z_tot: total non-dimensionalized (rho*c0) complex impedance of the sample (inverse of the total admittance)
+    alpha: total absorption of the sample
+    '''
+
+    Z = np.array([init_res(f,a_n = x[1],L_n = 1e-10,a_c = x[1],L_c = x[2]).Z for x in res_params]).transpose()
+    Z_tot = np.sum(np.round(res_params[:,0])/A_s*np.pi*res_params[:,1]**2*Z**-1,axis = -1).transpose()**-1
+    alpha = 1 - abs((Z_tot-1)/(Z_tot+1))**2
+
+    return Z_tot, alpha
+
+def opt_res(opt_in):
+
+    '''
+    This is the opjective function for the global  optimizer. Essentially the opjective is to minimize the normalized L2 error of the acoustic absorption.  
+    '''
+    # reshapes the array of input parameters so that each row corresponds to a different type of resonator
+    opt_in = opt_in.reshape(int(len(opt_in)/3),3,order = 'F')
+    
+    # initializes an zero array for the total smeered admittance (inverse of impedance)
+    Z_tot, alpha = smeared_Z(f,opt_in)
+    L2_err = np.sum(W*(1-alpha)**2)/np.sum(W)
+
+    print(L2_err)
+
+    if np.isnan(L2_err):
+        raise Exception('nan encountered')
+
+    return L2_err
 
 #%%
 
@@ -37,7 +86,7 @@ W = np.abs(h)
 # Total number of resonators in the test sample
 N = 25
 # number of different types of resonators within the sample
-n = 5
+n = 3
 
 # initial values of the resonators
 a_n_init,L_n_init,a_c_init,L_c_init = 0.003809,0.003809*4,0.003809,0.003809*4
@@ -47,33 +96,14 @@ opt_in = np.array([int(N/n),a_c_init,L_c_init]).repeat(n)
 opt_bounds = np.array([[1,25],[a0/(100*2*np.pi*f[-1]),a0/(2*np.pi*f[-1])],[a0/(2*np.pi*f[-1]),50*a0/(2*np.pi*f[-1])]]).repeat(n,axis = 0)
 
 def get_constraints(lb,ub):
-    constraints = NonlinearConstraint(fun = lambda x: [np.sum(np.round(x[:n])),np.sum(np.round(x[:n])*np.pi*x[n:n+n]**2)]+list(x[n:n+n]),lb =lb,ub = ub)
+    constraints = NonlinearConstraint(fun = lambda x: [np.sum(np.round(x[:n])),np.sum(np.round(x[:n])*np.pi*x[n:n+n]**2)],lb =lb,ub = ub)
     return constraints
 
-
-# opt_bounds = np.array([[1,5],[1e-10,a_n_init*10],[1e-10,a_n_init*100],[1e-10,a_n_init*10],[1e-10,a_n_init*100]]).repeat(n,axis = 0)
-
-L2_err_coll = []
-
-def opt_res(opt_in):
-    # reshapes the array of input parameters so that each row corresponds to a different type of resonator
-    opt_in = abs(opt_in).reshape(int(len(opt_in)/3),3,order = 'F')
-    # initializes an zero array for the total smeered admittance (inverse of impedance)
-    beta_tot = np.zeros(len(f))
-    for i in range(len(opt_in)):
-        res_temp = res.resonator(a_n = opt_in[i,1],L_n =1e-10,a_c = opt_in[i,1], L_c = opt_in[i,2])
-        res_temp.set_Z(f,model = 'k',rad = False,interior = False,loss = True,table = False)
-        beta_tot = beta_tot+np.round(opt_in[i,0])/A_s*np.pi*res_temp.a_n**2*(res_temp.Z)**-1
-
-    alpha = 1 - abs((beta_tot**-1-1)/(beta_tot**-1+1))**2
-    L2_err = np.sum(W*(1-alpha)**2)/np.sum(W)
-    print(L2_err)
-    if np.isnan(L2_err):
-        raise Exception('nan encountered')
-    return L2_err
-
-integ = np.ones(len(opt_in))*False
-integ[:n] = True
-res_optimal = differential_evolution(opt_res,bounds = opt_bounds,constraints =get_constraints([1,0]+list(np.ones(n)*a0/(100*2*np.pi*f[-1])),[N,A_s]+list(np.ones(n)*a0/(2*np.pi*f[-1]))),maxiter = 3,popsize=100,recombination=0.75,mutation = 0,updating = 'immediate',polish=True)
+start_t = time()
+res_optimal = differential_evolution(opt_res,bounds = opt_bounds,constraints =get_constraints([1,np.pi*(a0/(2*np.pi*f[-1]))**2],[N,A_s]),popsize=100, updating = 'immediate',polish=False,workers = 1)
+elapsed_t = time()-start_t
+print(elapsed_t)
 
 res_optimal.x
+
+/
